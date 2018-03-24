7fc2c6729d0cf04e20bfda19985a6f079a52e5e1
bvdv <bvdv2019@gmail.com>
2018-03-24 22:27:08 +0200

    Get lang

--- public/index.php
+++ public/index.php
@@ -1,5 +1,16 @@
 <?php
 
+function getLang($default) {
+    return
+        !empty($_GET['lang']) ? $_GET['lang'] :
+            (!empty($_COOKIE['lang']) ? $_COOKIE['lang'] :
+                 (!empty($_SESSION['lang']) ? $_SESSION['lang'] :
+                       !empty($_SERVER['HTTP_ACCEPT_LANGUAGE'] ?substr($_SERVER['HTTP_ACCEPT_LANGUAGE'], 0, 2) :
+                       $default)));
+}
+
 $name = $_GET['name'] ?? 'Guest';
+$lang = getLang('en');
+
 header('Bvdv:github');
-echo "Hi " . $name . "!";
+echo "Hi " . $name . "! Your lang is " . $lang;
