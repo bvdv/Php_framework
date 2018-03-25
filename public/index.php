132dd00d2802ab43ec9fe1f861769ef52e97f8ee
bvdv <bvdv2019@gmail.com>
2018-03-25 20:16:38 +0300

    get_name request class

--- public/index.php
+++ public/index.php
@@ -1,5 +1,11 @@
 <?php
 
+use Framework\Http\Request;
+
+chdir(dirname(__DIR__));
+require 'src/Framework/Http/Request.php';
+
+
 function getLang(array $get, array $cookie, array $session, array $server, $default) {
     return
         !empty($get['lang']) ? $get['lang'] :
@@ -13,5 +19,12 @@ session_start();
 $name = $_GET['name'] ?? 'Guest';
 $lang = getLang($_GET, $_COOKIE, $_SESSION, $_SERVER,'en');
 
+### Initialization
+
+$request = new Request();
+
+### Action
+
+$name = $request->getQueryParams()['name'] ?? 'Guest';
 header('Bvdv:github');
 echo "Hi " . $name . "! Your lang is " . $lang;
