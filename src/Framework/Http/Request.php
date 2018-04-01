d4208453d87af3012275b295d872c9337293c226
bvdv <bvdv2019@gmail.com>
2018-04-01 20:35:29 +0300

    Request with PSR-7

--- src/Framework/Http/Request.php
+++ src/Framework/Http/Request.php
@@ -2,9 +2,13 @@
 
 namespace Framework\Http;
 
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Message\StreamInterface;
+use Psr\Http\Message\UriInterface;
+
 class Request implements RequestInterface
 {
-    private $queryParams = [];
+    private $queryParams;
     private $parsedBody;
 
     public function __construct(array $queryParams = [], $parsedBody = null)
@@ -36,4 +40,31 @@ class Request implements RequestInterface
         $new->parsedBody = $data;
         return $new;
     }
+
+    public function getProtocolVersio() {}
+    public function withProtocolVersio() {}
+    public function getHeaders() {}
+    public function hasHeader($name) {}
+    public function getHeader($name) {}
+    public function getHeaderLine($name) {}
+    public function withHeader($name, $value) {}
+    public function withAddedHeader($name, $value) {}
+    public function withoutHeader($name) {}
+    public function getBody() {}
+    public function withBody(StreamInterface $body) {}
+    public function getRequestTarget() {}
+    public function withRequestTarget($requestTarget) {}
+    public function getMethod() {}
+    public function withMethod($method) {}
+    public function getUri() {}
+    public function withUri(UriInterface $uri, $preserveHost = false) {}
+    public function getServerParams() {}
+    public function getCookieParams() {}
+    public function withCookieParams(array $cookies) {}
+    public function getUploadedFiles() {}
+    public function withUploadedFiles(array $uploadedFiles) {}
+    public function getAttributes() {}
+    public function getAttribute($name, $default = null) {}
+    public function withAttribute($name, $value) {}
+    public function withoutAttribute($name) {}
 }
