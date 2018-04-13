a251149f6bf232bdba0add5527df16b0fbd11360
bvdv <bvdv2019@gmail.com>
2018-04-13 16:33:34 +0300

    Home, About class

--- src/App/Http/Action/AboutAction.php
+++ src/App/Http/Action/AboutAction.php
@@ -1,1 +1,13 @@
 <?php
+
+namespace App\Http\Action;
+
+use Zend\Diactoros\Response\HtmlResponse;
+
+class AboutAction
+{
+    public function __invoke()
+    {
+        return new HtmlResponse('About Us');
+    }
+}
