2e92b0604f4a2e1606b5d9d27cb6c82d0bda3d88
bvdv <bvdv2019@gmail.com>
2018-04-14 10:58:56 +0300

    Blog Show action

--- src/App/Http/Action/Blog/ShowAction.php
+++ src/App/Http/Action/Blog/ShowAction.php
@@ -1,1 +1,21 @@
 <?php
+
+namespace App\Http\Action\Blog;
+
+use Psr\Http\Message\ServerRequestInterface;
+use Zend\Diactoros\Response\HtmlResponse;
+use Zend\Diactoros\Response\JsonResponse;
+
+class ShowAction
+{
+    public function __invoke(ServerRequestInterface $request)
+    {
+        $id = $request->getAttribute('id');
+
+        if ($id > 5) {
+            return new HtmlResponse('B Page not Found', 404);
+        }
+
+        return new JsonResponse(['id' => $id, 'title' => 'Post #' . $id]);
+    }
+}
