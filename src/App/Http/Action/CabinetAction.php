da8334ceb01c38850f6bdd1493ebf12e382b640a
bvdv <bvdv2019@gmail.com>
2018-04-16 20:14:30 +0300

    add auth action

--- src/App/Http/Action/CabinetAction.php
+++ src/App/Http/Action/CabinetAction.php
@@ -3,12 +3,34 @@
 namespace App\Http\Action;
 
 use Psr\Http\Message\ServerRequestInterface;
+use Zend\Diactoros\Response\EmptyResponse;
 use Zend\Diactoros\Response\HtmlResponse;
 
 class CabinetAction
 {
-    public function __invok(ServerRequestInterface $request)
+    private $users;
+
+    public function __construct(array $users)
     {
-        return new HtmlResponse('Cabinet.');
+        $this->users = $users;
+    }
+
+    public function __invoke(ServerRequestInterface $request)
+    {
+
+        $username = $request->getServerParams()['PHP_AUTH_USER'] ?? null;
+        $password = $request->getServerParams()['PHP_AUTH_PW'] ?? null;
+
+        if (!empty($username) && !empty($password)) {
+            foreach ($this->users as $name => $pass) {
+                if ($username === $name && $password === $pass) {
+                    return new HtmlResponse('Logged as: ' . $username);
+                }
+            }
+
+        }
+
+        return new EmptyResponse(401, ['WWW-Authenticate' => 'Basic realm=Restricted area']);
+
     }
 }
