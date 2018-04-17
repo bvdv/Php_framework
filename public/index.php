d0ea51efe34dca814e3a64fa613d3f95f5c979f2
bvdv <bvdv2019@gmail.com>
2018-04-17 12:00:28 +0300

    Extend auth action

--- public/index.php
+++ public/index.php
@@ -21,7 +21,9 @@ $routes = $aura->getMap();
 
 $routes->get('home', '/', Action\HelloAction::class);
 $routes->get('about', '/about',Action\AboutAction::class);
-$routes->get('cabinet', '/cabinet', new Action\CabinetAction($params['users']));
+
+$routes->get('cabinet', '/cabinet', new Action\BasicAuthActionDecorator(new Action\CabinetAction(), $params['users']));
+
 $routes->get('blog', '/blog', Action\Blog\IndexAction::class);
 $routes->get('blog_show', '/blog/{id}', Action\Blog\ShowAction::class, ['id' => '\d+']);
 
