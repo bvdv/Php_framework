36e0b7deb613ae908066ecde938dda779d4cb30b
bvdv <bvdv2019@gmail.com>
2018-04-19 09:33:25 +0300

    index.php change for pipeline

--- public/index.php
+++ public/index.php
@@ -3,9 +3,11 @@
 use App\Http\Action;
 use App\Http\Middleware;
 use Framework\Http\ActionResolver;
+use Framework\Http\Pipeline\Pipeline;
 use Framework\Http\Router\AuraRouterAdapter;
 use Framework\Http\Router\Exception\RequestNotMatchedException;
 use Psr\Http\Message\ServerRequestInterface;
+use Zend\Diactoros\Response\HtmlResponse;
 use Zend\Diactoros\Response\JsonResponse;
 use Zend\Diactoros\Response\SapiEmitter;
 use Zend\Diactoros\ServerRequestFactory;
@@ -26,14 +28,15 @@ $routes->get('home', '/', Action\HelloAction::class);
 $routes->get('about', '/about',Action\AboutAction::class);
 
 $routes->get('cabinet', '/cabinet', function (ServerRequestInterface $request) use ($params) {
-    $profiler = new Middleware\ProfilerMiddleware;
-    $auth = new Middleware\BasicAuthMiddleware($params['users']);
-    $cabinet = new Action\CabinetAction();
+    $pipeline = new Pipeline();
 
-    return $profiler($request, function (ServerRequestInterface $request) use ($auth, $cabinet){
-        return $auth($request, function (ServerRequestInterface $request) use ($cabinet) {
-            return $cabinet($request);
-        });
+    $pipeline->pipe(new Middleware\ProfilerMiddleware());
+    $pipeline->pipe(new Middleware\BasicAuthMiddleware($params['users']));
+    $pipeline->pipe(new Action\CabinetAction());
+
+    return $pipeline($request, function () {
+        return new HtmlResponse('Undefined page', 404);
+
     });
 });
 
