0dbf52bb8a316e1584b0658e3fc0a7242962e4b9
bvdv <bvdv2019@gmail.com>
2018-04-23 22:13:26 +0300

    Not found page

--- src/App/Http/Action/Blog/ShowAction.php
+++ src/App/Http/Action/Blog/ShowAction.php
@@ -3,17 +3,16 @@
 namespace App\Http\Action\Blog;
 
 use Psr\Http\Message\ServerRequestInterface;
-use Zend\Diactoros\Response\HtmlResponse;
 use Zend\Diactoros\Response\JsonResponse;
 
 class ShowAction
 {
-    public function __invoke(ServerRequestInterface $request)
+    public function __invoke(ServerRequestInterface $request, callable $next)
     {
         $id = $request->getAttribute('id');
 
         if ($id > 5) {
-            return new HtmlResponse('B Page not Found', 404);
+            return $next($request);
         }
 
         return new JsonResponse(['id' => $id, 'title' => 'Post #' . $id]);
