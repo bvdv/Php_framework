8960f24d64bc2e5fc4a67fe002ca3e1a5bd8ebc0
bvdv <bvdv2019@gmail.com>
2018-04-26 22:14:03 +0300

    MiddlewareResolver correction

--- src/Framework/Http/Pipeline/MiddlewareResolver.php
+++ src/Framework/Http/Pipeline/MiddlewareResolver.php
@@ -8,11 +8,27 @@ class MiddlewareResolver
 {
     public function resolve($handler): callable
     {
+        if (\is_array($handler)) {
+            return $this->createPipe($handler);
+        }
+
         if (\is_string($handler)) {
             return function (ServerRequestInterface $request, callable $next) use ($handler) {
                 $object = new $handler();
                 return $object($request, $next);
             };
         }
+
+        return $handler;
+    }
+
+    private function createPipe(array $handlers): Pipline
+    {
+        $pipeline = new Pipeline();
+        foreach ($handlers as $handler) {
+            $pipeline->pipe($this->resolve($handler));
+        }
+
+        return $pipeline;
     }
 }
